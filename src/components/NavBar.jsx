f87705313b89850f23ca0b07d889491a476b33a8
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-06 14:29:39 -0300

    probando el css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
-import { CartWidget } from "./CartWidget";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export const NavBar = () => (
-<header>
-    <div> Indumentaria S.A. </div>
-    <nav>
-        <a href='#'>Hombres</a>
-        <a href='#'>Mujeres</a>
-        <a href='#'>Niños</a>
-    </nav>
-    <CartWidget/>
-</header>
+import { CartWidget } from "./CartWidget";
+export const Navbar = () => (
+      <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Indumentaria S.A.</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Hombres</Nav.Link>
+            <Nav.Link href="#features">Mujeres</Nav.Link>
+            <Nav.Link href="#pricing">Niños</Nav.Link>
+          </Nav>
+          <CartWidget/>
+        </Container>
+      </Navbar>
+      </>
 );
