73b14a7435f8b05222ffb8fe731589bfd1dc2136
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-06 12:46:48 -0300

    navbar armado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@
 import './App.css';
 
 function App() {
-  return <div> Hola Mundo! </div>;
+  return (
+    <header>
+  <div> Indumentaria S.A. </div>
+  <nav>
+    <a href='#'>Hombres</a>
+    <a href='#'>Mujeres</a>
+    <a href='#'>Niños</a>
+  </nav>
+  </header>);
 }
 
 export default App;
