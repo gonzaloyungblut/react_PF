e03d65c6a617d8ee97c64692ae9815c3a6679ba4
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-29 11:25:23 -0300

    fin del arrany json

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -15,29 +15,29 @@ export const ItemListContainer = (props) => {
             setTimeout(() => resolve(data), 2000);
         });
 
-        promise.then((data)=> setProducts(data));
+        promise.then((data) => setProducts(data));
 
     }, []);
 
     return (
-    <Container>
-        <h1>{props.greeting}</h1>
-        <div>
-            {products.map(product => (
+        <Container>
+            <h1>{props.greeting}</h1>
+            <div>
+                {products.map(product => (
 
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-            ))}
-        </div>
-    </Container>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+                ))}
+            </div>
+        </Container>
     );
 };
