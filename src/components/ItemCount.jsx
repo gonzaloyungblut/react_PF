5b719af7cc3b5e9921dd246efd99f87c8a88406c
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-29 12:13:32 -0300

    linkeando mas mejor

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
+const stock = 10;
+
 export const ItemCount = () => {
+const [count, setCount] = useState(1);
+
+const handleDecreaseCount = () => {
+    if (count > 1) {
+        setCount((prev) => prev - 1);
+    }
+};
+
+const handleIncreaseCount = () => {
+    if (stock > count) {
+        setCount((prev) => prev + 1);
+    }
+};
+
+const onAdd = () => {
+    alert(count);
+};
+
     return (
         <div className="ItemCount">
-            <span>-</span>
-            <span>0</span>
-            <span>+</span>
-            <button>Agregar al carrito</button>
+            <span onClick={handleDecreaseCount}>-</span>
+            <span>{count}</span>
+            <span onClick={handleIncreaseCount}>+</span>
+            <button onClick={onAdd}>Agregar al carrito</button>
         </div>
     );
 };
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,8 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-
-
 import data from "../data/products.json";
 
 export const ItemListContainer = (props) => {
