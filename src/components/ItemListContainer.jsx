9d8e80692f38fe45851c8f7001d0471272c2ef2c
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-29 10:45:26 -0300

    metiendole cartitas

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,10 +1,43 @@
+import { useState, useEffect } from "react";
 import Container from 'react-bootstrap/Container';
-import { ItemCount } from "./ItemCount";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+
+import data from "../data/products.json";
 
 export const ItemListContainer = (props) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve(data), 2000);
+        });
+
+        promise.then((data)=> setProducts(data));
+
+    }, []);
+
     return (
     <Container>
         <h1>{props.greeting}</h1>
-        <ItemCount/>
-    </Container>);
+        <div>
+            {products.map(product => (
+
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+            ))}
+        </div>
+    </Container>;
+    );
 };
