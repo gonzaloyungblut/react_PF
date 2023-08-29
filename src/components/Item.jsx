05e4bc67a5189949856a7fc973d5a8fa14034126
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-08-29 14:54:13 -0300

    acomodando el codigo

---
+++ src/components/Item.jsx
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export const Item = ({ product }) => (
+    <Card key={product.id} style={{ width: '18rem' }}>
+        <Card.Img style={{height: "200px" }} variant="top" src={product.avatar} />
+        <Card.Body>
+            <Card.Title>{product.name}</Card.Title>
+            <Card.Text>
+                {product.category}
+            </Card.Text>
+            <Button variant="primary">Ver más</Button>
+        </Card.Body>
+    </Card>
+);
