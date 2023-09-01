d61757616b2af89809431aa6131b9a16a573b9b8
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-09-01 17:04:40 -0300

    mal tipeo

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -8,14 +8,18 @@ import data from "../data/products.json";
 export const ItemListContainer = (props) => {
     const [products, setProducts] = useState([]);
 
+    const { id } = useParams();
+
     useEffect(() => {
         const promise = new Promise((resolve, reject) => {
             setTimeout(() => resolve(data), 2000);
         });
 
-        promise.then((data) => setProducts(data));
-
-    }, []);
+        promise.then((data) => {
+            if (!id) { setProducts(data);} else { 
+                const productsFiltered = data.filter((product)=>product.category === id);
+            setProducts(productsFiltered);}
+    });}, []);
 
     return (
         <Container>
