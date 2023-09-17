2cd836ac83b4624836170ed664f9de5019908bbf
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-09-17 17:10:19 -0300

    modificando item detail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,20 @@
-export const ItemDetail = ({ product }) => (
+import { useContext } from "react";
+import { ItemCount } from "./ItemCount";
+import { CartContext } from "../contexts/CartContext";
+
+
+
+export const ItemDetail = ({ product }) => {
+    const { addItem } = useContext(CartContext)
+    const onAdd = count => addItem(product, count)
+
+    return(
     <div style={{ display: "flex", flexWrap: "wrap" }} >
         <h1>{product.name} </h1>
         <img src={product.avatar} alt={product.category} />
-        <div>{product.stock}</div>
+        <div> Stock: {product.stock}</div>
+        <div> Precio: {product.precio} </div>
+        <ItemCount stock={product.stock} onAdd={onAdd} />
     </div>
 
-);
+)};
