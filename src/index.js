70ddc897db8c9a0c33cc5710bc85a4306e342c9f
Gonzalo Yungblut <gonzaloyungblut@gmail.com>
2023-09-18 11:10:11 -0300

    firestone

--- src/index.js
+++ src/index.js
@@ -5,6 +5,28 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAC_3a1pwk--AL3I7mEOisWIp17ZD1_H3c",
+  authDomain: "react-yungblut.firebaseapp.com",
+  projectId: "react-yungblut",
+  storageBucket: "react-yungblut.appspot.com",
+  messagingSenderId: "811166618397",
+  appId: "1:811166618397:web:2063ba86dac6cecd5407f4",
+  measurementId: "G-NSG7R579VH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
